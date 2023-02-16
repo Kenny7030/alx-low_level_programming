c508b7ab1643f02726c0717707ae30eef77a82ad
kenny7030 <kariukikennedy6@gmail.com>
2023-02-16 03:16:58 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#iclude <stdio.h>
+/**
+ * main - ENTRY POINT
+ * RETURN: ALWAYS 0 (SUCCESS)
+ */
+
+int main(void)
+{	
+	puts("\"programming is like building a multilingual puzzle")"
+	     return (0);
+}

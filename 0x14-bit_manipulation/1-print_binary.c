5bd508ff6df19226665af2f13d216ae54139e423
kenny7030 <kariukikennedy6@gmail.com>
2023-04-10 11:27:51 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * print_binary - Prints a binary notation of a number.
- * @n: number to be printed in binary.
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n >> 0)
 	{
-		print_binary(n >> 1);
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	putchar((n & 1) + '0');
+	else
+	{
+		_putchar('0');
+	}
 }
-
-int main(void)
-{
-	unsigned long int number = 42;
-	printf("The binary representation of %lu is: ", number);
-	print_binary(number);
-	printf("\n");
-	return 0;
-}
-

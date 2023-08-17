8134329ecfdcb55f46588813d6a672697a976ea0
kenny7030 <kariukikennedy6@gmail.com>
2023-08-17 13:54:14 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,29 +8,28 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+    dlistint_t *new = malloc(sizeof(dlistint_t));
     dlistint_t *current = *head;
 
-    if (new_node == NULL)
-        return NULL;
+    if (new == NULL)
+        return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+    new->n = n;
+    new->next = NULL;
 
-    if (*head == NULL) // If the list is empty
+    if (current != NULL)
     {
-        new_node->prev = NULL;
-        *head = new_node;
-        return new_node;
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+    else
+    {
+        *head = new;
     }
 
-    // Traverse to the last node
-    while (current->next != NULL)
-        current = current->next;
+    new->prev = current;
 
-    current->next = new_node;
-    new_node->prev = current;
-
-    return new_node;
+    return new;
 }
 
